25a68eca5fb2aa273bd9eab8f4b255f4e411ce5f
umeh-promise <umehpromise1000@gmail.com>
2025-03-17 00:48:27 +0100

    Add new icon components for Store, NotesTick, Optimize, MedicalBag, TabReview, Map, MarketingField, Public, MoneyBag, Service, Seo, and SearchZoomIn

---
+++ src/icons/optimze.tsx
@@ -0,0 +1,26 @@
+export function OptimzeIcon() {
+  return (
+    <svg
+      width="68"
+      height="68"
+      viewBox="0 0 68 68"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M26.9154 11.334L39.6654 22.6673L53.8774 14.3232L46.7487 29.7507L59.4987 41.084L42.4987 39.6673L36.1237 53.834L32.582 38.2507L15.582 36.834L30.4684 27.8382L26.9154 11.334Z"
+        fill="#385065"
+        stroke="#385065"
+        strokeWidth="5.66667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.332 59.5293L32.582 38.251"
+        stroke="#385065"
+        strokeWidth="5.66667"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
