14de824a49577a78bb477282d0f76b5c06b17fe2
jesseamos <anietiejesse@gmail.com>
2025-03-25 18:00:31 +0100

    added favicon iimage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,9 @@ export const metadata: Metadata = {
     default: "4E Agency",
   },
   description: "An Agency That Gets Your Brand What it Deserves",
+  icons:{
+    icon:"/favicon.png"
+  }
 };
 
 export default function RootLayout({
